2cb6ae0ef11715353d390c8cb3c2e7f6d7b67929
snrnapa <shino.satoru@gmail.com>
2024-03-09 00:19:58 +0900

    APIを暫定的に作成した

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,20 +2,35 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
-@RequestMapping("/sample")
+// restControllerであることの宣言　ざっくり、「APIであること宣言する」ぐらいの解釈でいいと思う。
+// RESTAPIという単語を知ってると理解が早いが、知らなければ、調べるより、手を動かしたほうがいい。
+@RestController
+// URLに/userと入力することで、このclass(ここでいうと、HelloControllerクラス)に結び付くよ、というおまじない
+@RequestMapping("/user")
 public class HelloController {
 
-	@RequestMapping("/hello")
-	private String hello() {
-		System.out.println("helloをとらえました");
-		return "hello.html";
+	// "Get"Mappingであることに注意。これは、「/getというパスに、GETリクエストが飛んでくると、この中身が動くよ」というおまじない。正確に言うと、上で指定したものを加味して「/user/get」である。
+	@GetMapping("/get")
+	// 返却する値をJSONで返せるようにいい感じにしてくれるおまじない。JSONというものは、形だけ知ってほしい。活用場所は、まだ知らなくていい。
+	@ResponseBody
+	public String getAllUser() {
+		String result = "Stringを返します";
+		return result;
+
 	}
 
-	@RequestMapping("/bravo")
-	private String bravo() {
-		System.out.println("bravo");
-		return "bravo.html";
+	// user/test というパスに、GETリクエストを送ると、実行される。ここは、返値が「void」であるため、返値無し。
+	// さらに、メソッドにparamという変数名で、String型を渡すように言われている。たとえのリクエストを張っておくので、これをブラウザに突っ込んでほしい。
+	// http://localhost:8080/user/test?param=くええええええええ
+	@GetMapping("/test")
+	public void testMethod(@RequestParam String param) {
+		System.out.println("----------次の行で、引数を出力します。");
+		System.out.println(param);
 	}
+
 }
